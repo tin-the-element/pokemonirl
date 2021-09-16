77040dcea50136ca4baf275b18fb5a9e896fd195
tin-the-element <trungtin.luu@gmail.com>
2021-09-16 04:19:18 -0700

    Task complete with checkmark

--- src/sections/Tasks/ListTasks.js
+++ src/sections/Tasks/ListTasks.js
@@ -8,14 +8,16 @@ import {
   Link,
   useHistory
 } from "react-router-dom";
-
+import { Auth } from "@aws-amplify/auth"
 import { withAuthenticator } from '@aws-amplify/ui-react'
 import awsExports from "../../aws-exports";
+import { ReactComponent as Completed} from '../../assets/navbar_icons/check.svg'
 Amplify.configure(awsExports);
 
 const ListTasks = () => {
   const [singleTasks, setSingleTasks] = useState([])
   const [multiTasks, setMultiTasks] = useState([])
+  const [finishedTasks, setFinishedTasks] = useState([])
   const history = useHistory();
 
   useEffect(() => {
@@ -25,12 +27,22 @@ const ListTasks = () => {
 
   async function fetchProblems() {
     try {
+
+      const authUser = await Auth.currentAuthenticatedUser()  
+      const email = await authUser.attributes.email
+      
+
+      const userData = await API.graphql({query: queries.getAccount, variables: {id: email}})
+      const oldAccountData = userData.data.getAccount
+      const finishedTasks = oldAccountData.completed_tasks
+
       const singleTaskData = await API.graphql(graphqlOperation(queries.listSingleTasks))
       console.log(singleTaskData);
       const singleTasks = singleTaskData.data.listSingleTasks.items
 
       const multiTaskData = await API.graphql(graphqlOperation(queries.listMultipleTasks))
       const multiTasks = multiTaskData.data.listMultipleTasks.items
+      setFinishedTasks(finishedTasks)
       setSingleTasks(singleTasks)
       setMultiTasks(multiTasks)
     } catch (err) { console.log(err) }
@@ -58,7 +70,10 @@ const ListTasks = () => {
       {
         singleTasks.map((task, index) => (
           <div className="battle_item center-div" onClick={() => toSingle(task.id)} key={task.id ? task.id : index}>
+
+            {finishedTasks.includes(task.id) ? <Completed className="battle_item_completed"/>: <></>}
             <h3>{task.name}</h3>
+            {/* <Completed className="battle_item_completed_padding"/> */}
             <p className="battle_item_text">{task.question}</p>
             <img style={{height: '100px', width: '150px', objectFit: 'cover'}} alt={task.name} src={'/assets/single_tasks/' + task.images + '.jpg'} />
             <p>Exp Given: {task.exp_given}</p>
